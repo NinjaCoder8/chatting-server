6f14e837a5d9102f38de7d4bcf84bfce1416cd24
Charbel <charbel.dawoud@gmail.com>
2024-12-18 18:36:53 +0100

    PHP <> Implemented an API to get all users + filtered by gender

---
+++ apis/getAllUsers.php
@@ -0,0 +1,23 @@
+<?php 
+include("../db_connection/db_info.php");
+
+if(isset($_GET["gender"]) && $_GET["gender"] >= 0 && $_GET["gender"] <= 2){
+    $gender = $_GET["gender"];
+    $query = $mysqli->prepare("Select * FROM users WHERE gender = ?");
+    $query->bind_param("i", $gender);
+}else{
+    $query = $mysqli->prepare("Select * FROM users;");
+}
+
+$query->execute();
+$array = $query->get_result();
+
+$array_response = []; 
+while ($user = $array->fetch_assoc()){
+    $array_response[] = $user;
+}
+
+$json_response = json_encode($array_response);
+echo $json_response;
+
+?>
